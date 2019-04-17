339ec477d9e1e847824b3c7c89997b4f6e5db74b
Matthew Paletta <mattpaletta@gmail.com>
2019-04-16 17:32:34 -0700

    Fixed wrong file

--- test_redis.py
+++ test_redis.py
@@ -1,12 +1,13 @@
 import concurrent
 import os
+import sys
 import tempfile
 from concurrent.futures import ThreadPoolExecutor, Future
 from typing import List
 import logging
+import redis
 
 from azure.storage.blob import BlockBlobService, Blob
-from dask.distributed import get_client, secede, rejoin
 
 from pynotstdlib.logging import default_logging
 
@@ -14,15 +15,10 @@ container_name = "myblobs"
 account_name = "jgoddingblob"
 account_key = "rzfvY+HXTZGKJs5MMon3PHHKKtdBL9eZnKBw3tuav62xczvPNpJjp2D2Qg/bSyucTdVwNYbSu1veAF293ufWWQ=="
 
-import logging
-default_logging(logging.DEBUG)
+default_logging(logging.INFO)
 
 
 def process_file(folder_blob: str):
-    import logging
-    import sys
-    import os
-
     root = logging.getLogger()
     root.setLevel(logging.ERROR)
 
@@ -67,24 +63,28 @@ def process_file(folder_blob: str):
         import time
         retry=0
 
-        #while not os.path.exists(target_output_file):
-            #try:
-        block_blob_service.get_blob_to_path(container_name,
+        while not os.path.exists(target_output_file):
+            try:
+                block_blob_service.get_blob_to_path(container_name,
                                                     blob_name = sub_blob_file.name,
                                                     file_path = target_output_file,
                                                     validate_content=True)
+            except Exception:
+                time.sleep(2 ** retry)
+                retry += 1
+
         print("Downloaded: {0}".format(sub_blob_file.name))
 
     print("Target_root_folder: {0}".format(target_root_folder))
     # Download them in parallel, 30 at a time (so it's faster).
-    #with ThreadPoolExecutor(max_workers = 5) as executor:
-    #    task_list: List[Future] = []
-    for sub_blob_file in block_blob_service.list_blobs(container_name, prefix = folder_blob):
-        download_file(sub_blob_file)
-        #        task_future = executor.submit(download_file, sub_blob_file)
-    #        task_list.append(task_future)
+    with ThreadPoolExecutor(max_workers = 5) as executor:
+        task_list: List[Future] = []
+        for sub_blob_file in block_blob_service.list_blobs(container_name, prefix = folder_blob):
+            # download_file(sub_blob_file)
+            task_future = executor.submit(download_file, sub_blob_file)
+            task_list.append(task_future)
 
-     #   concurrent.futures.wait(task_list)
+       concurrent.futures.wait(task_list)
 
     print("Finished downloading files")
     sys.stdout.flush()
@@ -102,7 +102,6 @@ def process_file(folder_blob: str):
         # We already have a file, delete it
         os.remove(os.path.join(output_dir, output_file[0]))
     
-    client = get_client()
     print(psutil.virtual_memory())
 
     level1 = Level1(target_root_folder)
@@ -113,16 +112,11 @@ def process_file(folder_blob: str):
                     datasets = default_datasets + ['SPM'])
 
     print("'running' polymer")
-    
-    secede()  # Leave the worker pool
 
     # 0 for single threaded
     # > 1 for that many threads
     # -1 for all the threads on the machine.
-    a = client.submit(lambda: run_atm_corr(level1, level2, multiprocessing = 0))
-
-    rejoin()
-    a.result()
+    run_atm_corr(level1, level2, multiprocessing = -1)
 
     # The output file is the last thing in the path of the target_root_folder
     output_file = list(filter(lambda f: f.startswith(target_root_folder.split("/")[-1]), os.listdir(output_dir)))[0]
@@ -134,18 +128,6 @@ def process_file(folder_blob: str):
                                              blob_name = os.path.join("output", output_file), 
                                              file_path = os.path.join(output_dir, output_file),
                                              validate_content = True)
-
-
-def list_root_dir():
-    block_blob_service = BlockBlobService(account_name = account_name, account_key = account_key)
-
-    print("List blobs in the container: {0}".format(container_name))
-
-    # List up to the first folder (the 'sen3' files)
-    list_dir: List[Blob] = list(block_blob_service.list_blobs(container_name = container_name, prefix = "raw/", delimiter = "/"))
-    print("Found {0} files".format(len(list_dir)))
-    return list_dir
-
 
 def update_paths():
     import os
@@ -167,52 +149,17 @@ def update_paths():
     sys.path.append(cwd)
     import polymer.main
 
-# def process_test(my_input):
-#     print("my_input" + str(my_input))
-#     return my_input
 
+def run_worker():
+    update_paths()
 
-def run_dask():
-    # TODO: Add dask as separate main function
-    from dask.distributed import Client
-    client = Client()  # start local workers as processes
-    #client.upload_file('worker_script.py')
-    
-    client.run(update_paths)
-    print("Distributed file")
+    redis_conn = redis.StrictRedis(host = os.environ["SCHEDULER_HOST_PORT"], port = 6379, encoding = "utf-8")
 
-    list_dir = list_root_dir()
-    file_names = list(map(lambda f: f.name, list_dir))[:-1]
-    print(file_names)
-    
-    # Nuke the folder before submitting new work
-    #update_paths()
-    #list(map(process_file, ["raw/S3A_OL_1_EFR____20160802T190118_20160802T190318_20171002T220249_0119_007_113______MR1_R_NT_002.SEN3/"]))
-
-    result = client.map(process_file, file_names)
-    #result = []
-    #for f in file_names:
-    #    future = client.submit(process_file, f)
-    #    result.append(future.result())
-
-    #result = future.result()
-    #print(result)
-    #for f in result:
-    #    f.result()
-    print(client.gather(result))
+    while True:
+        folder_to_process = redis_conn.blpop("geo-queue", timeout = 0)
+        logging.info("Got folder: {0}".format(folder_to_process))
+        process_file(folder_to_process)
 
 
 if __name__ == "__main__":
-    run_dask()
-   #run_spark()
-
-# def run_spark():
-#     from pyspark import SparkContext
-#     # spark = SparkContext(master = "local[*]", appName = "GeoProcessing").getOrCreate()
-#     spark = SparkContext(master = "spark://mattTest-int.azurehdinsight.net:7077", appName = "GeoProcessing").getOrCreate()
-#
-#     list_dir = list_root_dir()
-#
-#     # for f in list_dir:
-#     #     process_file(f)
-#     spark.parallelize(list_dir).map(process_file).collect()
+    run_worker()
